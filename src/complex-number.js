2902c4435bce73a2c68d770c192f5645cb9a8291
Sourov <SourovKH@users.noreply.github.com>
2023-05-03 10:05:07 +0530

    added a function and its test to add two complex numbers

--- src/complex-number.js
+++ src/complex-number.js
@@ -10,4 +10,16 @@ const complexNumber = function(real, imaginary) {
   return {getRealPart, getImaginaryPart};
 }
 
+const operationOnComplexNumbers = function() {
+  const add = function(number1, number2) {
+    const realPart = number1.real + number2.real;
+    const imaginaryPart = number1.imaginary + number2.imaginary;
+
+    return {realPart, imaginaryPart};
+  }
+
+  return {add};
+}
+
 exports.complexNumber = complexNumber;
+exports.operationOnComplexNumbers = operationOnComplexNumbers;
--- test/complex-number-test.js
+++ test/complex-number-test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const deepStrictEqual = require('assert');
 const {describe, it} = require('node:test');
 
-const {complexNumber} = require("../src/complex-number.js");
+const {complexNumber, operationOnComplexNumbers} = require("../src/complex-number.js");
 
 describe("complexNumber", function() {
   it("should give the real part of given imaginary number", function() {
@@ -17,5 +17,12 @@ describe("complexNumber", function() {
     const imaginaryPart = 3;
     const complex = complexNumber(2, 3);
     deepStrictEqual(3, complex.getImaginaryPart());
+  });
+
+  it("should give the addition of two given complex numbers", function() {
+    const number1 = {realPart: 2, imaginaryPart: 3};
+    const number2 = {realPart: 1, imaginaryPart: 4};
+    const operation = operationOnComplexNumbers();
+    deepStrictEqual({realPart: 3, imaginaryPart: 7}, operation.add(number1, number2));
   });
 });
